4d513a7726b17e00fd3ef3c012c0964923cfb93f
Stefan Rimaila <stefan@rimaila.fi>
2018-01-02 14:30:31 +0200

    Fix broken test

--- src/shared/utils.test.js
+++ src/shared/utils.test.js
@@ -11,8 +11,10 @@ describe('shared/utils', () => {
               x => {
                 const r1 = H.camelCase(x);
                 const r2 = R.compose(R.join(''),
+                                     R.adjust(R.toLower, 0),
                                      R.map(R.pipe(R.adjust(R.toUpper, 0), R.join(''))),
                                      R.split('-'))(x);
+
                 return r1 === r2;
               });
 });
